da34ef0d221852d6600bc17e0644de6d2a9f74c8
Uliana Blinova <ulanablinova0@gmail.com>
2025-11-12 23:38:23 +0700

    past 5:Check yourself

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -8,16 +6,33 @@ def index(request):
     Функция отображения для домашней страницы сайта.
     """
     # Генерация "количеств" некоторых главных объектов
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    # Количество жанров
+    num_genres = Genre.objects.count()
+
+    # Количество книг
+    search_word = "war"  # вы можете изменить это слово
+    num_books_with_word = Book.objects.filter(title__icontains=search_word).count()
+
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        'catalog/index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            # --- НОВОЕ ---
+            'num_genres': num_genres,
+            'num_books_with_word': num_books_with_word,
+            'search_word': search_word,
+        },
     )
