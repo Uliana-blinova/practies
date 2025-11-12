61b2b95dd7bcdf053b280ca77c09c79ed6ad2801
Uliana Blinova <ulanablinova0@gmail.com>
2025-11-12 17:32:53 +0700

    model language

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -71,3 +71,35 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '%s (%s)' % (self.id,self.book.title)
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '%s, %s' % (self.last_name, self.first_name)
+class Language(models.Model):
+    """
+    Model representing a language (e.g., English, French, Spanish).
+    """
+    name = models.CharField(max_length=100, help_text="Enter the language (e.g., English, French, Spanish)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (e.g., Language object).
+        """
+        return self.name
