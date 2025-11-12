818538a63650b219eec770077c1c662dbfca9887
Uliana Blinova <ulanablinova0@gmail.com>
2025-11-12 18:54:01 +0700

    add filter for book instance

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,14 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
+#admin.site.register(Book)
 #admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+# Register the Admin classes for Book using the decorator
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+admin.site.register(Book, BookAdmin)
+# Register the Admin classes for BookInstance using the decorator
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+admin.site.register(BookInstance, BookInstanceAdmin)
