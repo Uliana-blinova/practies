a7d5c7c08153ac7493f11cedea2d63cb9e7ba45a
Uliana Blinova <ulanablinova0@gmail.com>
2025-11-12 18:08:23 +0700

    add class AuthorAdmin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
